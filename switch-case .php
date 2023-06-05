2598b4d7dc858184291cdc5148b02b594ec86e9f
TranTheSon2206 <109707502+TranTheSon2206@users.noreply.github.com>
2023-06-05 10:09:41 +0700

    Update switch-case .php

--- switch-case .php
+++ switch-case .php
@@ -1,3 +1,20 @@
+Câu lệnh switch-case trong PHP được sử dụng để kiểm tra một biến hoặc giá trị và thực hiện các hành động khác nhau dựa trên giá trị đó.
+đây là cú pháp của câu lệnh switch-case trong PHP:
+
+    switch (expression) {
+    case value1:
+        // Thực hiện hành động 1
+        break;
+    case value2:
+        // Thực hiện hành động 2
+        break;
+    //...
+    default:
+        // Thực hiện hành động mặc định nếu không có case nào khớp
+        break;
+}
+
+
 <?php
 $a = 0.5;
 $check = is_int($a); // Kiểm tra $a có phải là số nguyên không
